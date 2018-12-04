31884e4976b95b2f5d79d601cca9283c71397a2b
Teresa Knowles <teresa07moreno@gmail.com>
2018-12-04 11:24:21 -0700

    Adds ship class

---
+++ lib/ship.rb
@@ -0,0 +1,26 @@
+class Ship
+  attr_reader :name,
+              :length,
+              :health
+
+  def initialize(name, length)
+    @name = name
+    @length = length
+    @health = length
+
+  end
+
+  def sunk?
+    health == 0
+  end
+
+  def hit
+    if health > 0
+      @health -= 1
+    else
+      health
+    end
+  end
+
+
+end

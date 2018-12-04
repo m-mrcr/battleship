e1f06c44fc7f8200f6f19359c2bb576f617ffdf6
Martin Mercer <martinmercer1@gmail.com>
2018-12-04 11:22:56 -0700

    add ship class + test

---
+++ lib/ship.rb
@@ -0,0 +1,28 @@
+class Ship
+
+  attr_reader :name,
+              :length,
+              :health
+
+
+  def initialize(name, length)
+    @name = name
+    @length = length
+    @health = length
+  end
+
+  def hit
+    if health > 0
+      @health -= 1
+    else
+      health
+    end
+  end
+
+  def sunk?
+    health == 0
+  end
+
+
+
+end

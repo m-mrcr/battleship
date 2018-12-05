7ff841853149bd50900868b9b1fafd78703930e3
Teresa Knowles <teresa07moreno@gmail.com>
2018-12-05 16:04:40 -0700

    Adds tests for board cells method

--- test/board_test.rb
+++ test/board_test.rb
@@ -3,6 +3,7 @@ require 'minitest/pride'
 require 'pry'
 require './lib/cell'
 require './lib/ship'
+require './lib/board'
 
 class BoardTest < MiniTest::Test
   def test_it_exists
@@ -11,17 +12,19 @@ class BoardTest < MiniTest::Test
   end
 
   def test_that_cells_are_a_hash
-    skip
+
     board = Board.new
     assert_instance_of Hash, board.cells
   end
 
   def test_that_there_16_cells_in_board
+
     board = Board.new
     assert_equal 16, board.cells.count
   end
 
   def test_that_coordinates_map_to_cells
+    
     board = Board.new
     board.cells.values.all? do |value|
       value.class == Cell

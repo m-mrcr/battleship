94da1845eff2e552af37324f67dce29ad0b91d28
Teresa Knowles <teresa07moreno@gmail.com>
2018-12-05 16:15:56 -0700

    Changes cells method ranges to its own variables to help with scalability

--- lib/board.rb
+++ lib/board.rb
@@ -2,12 +2,16 @@ class Board
 
   def cells
     cells = Hash.new
-    ("A".."D").each do |letter|
-      (1..4).each do |number|
-        coordinate = "#{letter}#{number}"
-        cells[coordinate] = Cell.new(coordinate)
-    end
-  end
+    letter_coordinates = ("A".."D").to_a
+    number_coordinates = (1..4).to_a
+
+      letter_coordinates.each do |letter|
+
+        number_coordinates.each do |number|
+          coordinate = "#{letter}#{number}"
+          cells[coordinate] = Cell.new(coordinate)
+        end
+      end
   cells
   end
 end

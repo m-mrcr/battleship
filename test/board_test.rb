b787c75dcd13453127f187b7580e899c54e68573
Teresa Knowles <teresa07moreno@gmail.com>
2018-12-06 15:30:21 -0700

    Finishes tests for consecutive placements

--- test/board_test.rb
+++ test/board_test.rb
@@ -94,10 +94,10 @@ class BoardTest < MiniTest::Test
     submarine = Ship.new("Submarine", 2)
 
     assert_equal false, board.valid_placement?(cruiser, ["A1", "A2", "A4"])
-    # assert_equal false, board.valid_placement?(submarine, ["A1", "C1"])
+    assert_equal false, board.valid_placement?(submarine, ["A1", "C1"])
 
-    # assert_equal false, board.valid_placement?(cruiser, ["A3", "A2", "A1"])
-    # assert_equal false, board.valid_placement?(submarine, ["C1", "B1"])
+    assert_equal false, board.valid_placement?(cruiser, ["A3", "A2", "A1"])
+    assert_equal false, board.valid_placement?(submarine, ["C1", "B1"])
   end
 
 

d19bcac213e915f4250e0e4d6f6ebee00dace64a
Teresa Knowles <teresa07moreno@gmail.com>
2018-12-06 16:04:51 -0700

    Fixes test for place method

--- test/board_test.rb
+++ test/board_test.rb
@@ -103,11 +103,14 @@ class BoardTest < MiniTest::Test
   def test_it_can_place_a_ship
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
-    board.place(cruiser, ["A1", "A2", "A3"])
 
     cell_1 = board.cells["A1"]
     cell_2 = board.cells["A2"]
     cell_3 = board.cells["A3"]
+    binding.pry
+
+    board.place(cruiser, ["A1", "A2", "A3"])
+
 
     assert_equal cruiser, cell_1.ship
     assert_equal cruiser, cell_2.ship

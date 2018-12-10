a6e1da4d7eda9d9f97e2ca50d97956c61a0697ef
Teresa Knowles <teresa07moreno@gmail.com>
2018-12-09 19:42:02 -0700

    changes place ships method to make it simpler

--- lib/game.rb
+++ lib/game.rb
@@ -1,36 +1,40 @@
+require './lib/cell'
+require './lib/ship'
 require './lib/player'
 require './lib/board'
 
 class Game
 
   attr_reader :computer,
-              :human
+              :human,
+              :valid
 
   def initialize
-    welcome
+    @computer = Player.new('computer')
+    @human = Player.new('human')
   end
 
-  def welcome
-
-    puts "Welcome to BATTLESHIP"
-    puts "Enter p to play. Enter q to quit"
-    input = gets.chomp
-
-
-      if input == "p" || input == "P"
-        self.start_game
-      elsif input == 'q' || input == "Q"
-        exit
-      else
-        puts 'INVALID INPUT'
-        self.welcome
-      end
-  end
+  # def welcome
+  #
+  #   puts "Welcome to BATTLESHIP"
+  #   puts "Enter p to play. Enter q to quit"
+  #   input = gets.chomp
+  #
+  #
+  #     if input == "p" || input == "P"
+  #       self.start_game
+  #     elsif input == 'q' || input == "Q"
+  #       exit
+  #     else
+  #       puts 'INVALID INPUT'
+  #       self.welcome
+  #     end
+  # end
 
   def start_game
     @computer = Player.new(:computer)
     @player = Player.new(:human)
-    self.place_ships_ai
+
 
   end
 
@@ -38,81 +42,24 @@ class Game
 
   def place_ships_ai
 
-    # valid = false
-    # while valid == false
+    trial_coordinates = []
 
-      @computer.ships.each do |ship|
-        initial_cell = @computer.board.cells.values.sample
-        orientation = [:horizontal, :vertical]
+    @computer.ships.each do |ship|
 
+      trial_coordinates = @computer.board.cells.keys.sample(ship.length)
 
-          if orientation.sample == :horizontal
-            trial_coordinates = get_horizontal_coordinates(initial_cell, ship)
-            binding.pry
+      while @computer.board.valid_placement?(ship, trial_coordinates) == false
+        trial_coordinates = @computer.board.cells.keys.sample(ship.length)
+      end #ends while
 
-            if trial_coordinates.all? do |coordinate|
-              @computer.board.valid_coordinate?(coordinate)
-            end
-
-              valid_coordinates = trial_coordinates
-                if @computer.board.valid_placement?(ship, valid_coordinates)
-                  @computer.board.place(ship, valid_coordinates)
-                else
-                  binding.pry
-                  self.place_ships_ai
-                end
-            end
+    @computer.board.place(ship, trial_coordinates)
+  end #ends each
+  end #end of place ships ai method
 
 
 
-
-          else
-            get_horizontal_coordinates(initial_cell, ship)
-          end
-
-      end
-    # end
-
-  end
+end #ends class
 
 
-
-def get_horizontal_coordinates(initial_cell, ship)
-  columns = []
-
-  letter = initial_cell.coordinate.chars[0]
-  initial_column = initial_cell.coordinate.chars[1].to_i
-  columns << initial_column
-
-
-  if [:east, :west].sample == :east
-    until ship.length == columns.length
-      column = initial_column + 1
-      columns << column
-    end
-
-
-  else
-    until ship.length == columns.length
-      column = initial_column - 1
-      columns << column
-    end
-  end
-
-columns = columns.map do |column|
-  column.to_s
-end
-
-coordinates = columns.map do |number|
-  letter + number
-end
-
-
-
-
-end
-
-
-end
 
 binding.pry

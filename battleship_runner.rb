ee88bbacba2454bc704ca9be3732fa798026d3e6
Martin Mercer <martinmercer1@gmail.com>
2018-12-10 17:54:19 -0700

    creates the runner for our game

---
+++ battleship_runner.rb
@@ -0,0 +1,21 @@
+require './lib/cell'
+require './lib/ship'
+require './lib/turn'
+require './lib/player'
+require './lib/board'
+require 'pry'
+
+game = Game.new
+game.setup
+# binding.pry
+turn = Turn.new(game.human, game.computer)
+loop do
+  turn.display_boards
+  turn.player_chooses_coordinate
+  turn.computer_chooses_coordinate
+
+  if turn.computer_won == true || turn.player_won == true
+    turn.end_game_announcement
+  break
+  end
+end

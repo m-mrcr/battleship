4eb43ebd9f1de6da1e995a43b5128944f4e13132
Teresa Knowles <teresa07moreno@gmail.com>
2018-12-12 11:26:31 -0700

    adds test for place ships ai

--- test/game_setup_test.rb
+++ test/game_setup_test.rb
@@ -5,24 +5,34 @@ require './lib/cell'
 require './lib/ship'
 require './lib/board'
 require './lib/game_setup'
-require './lib/player'
 
-class GameTest < MiniTest::Test
+class GameSetupTest < MiniTest::Test
 
   def test_it_exists
-    game = GameSetup.new
 
-    assert_instance_of Game, game
+    game = GameSetup.new
+    assert_instance_of GameSetup, game
+
   end
 
-  def test_it_has_a_human_player_and_a_computer_player
+  def test_it_has_a_human_and_a_computer_that_are_players
     game = GameSetup.new
 
-    computer = Player.new(:computer)
-    human = Player.new(:human)
+    assert_instance_of Player, game.computer
+    assert_instance_of Player, game.human
 
-    assert_equal computer_1, game.computer
-    assert_equal human, game.human
+  end
+
+  def test_all_ships_of_computer_get_placed
+    skip
+    game = GameSetup.new
+
+    game.place_ships_ai
+
+    expected = 5
+    actual = game.computer.board.cells.values.count {|cell| !cell.empty? }
+
+    assert_equal expected, actual
   end
 
 

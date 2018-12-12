fe42aceb70d7205cfffd05ba1f711d64ebcf6bef
Martin Mercer <martinmercer1@gmail.com>
2018-12-12 16:48:45 -0700

    added instance assertion to the file

--- test/player_test.rb
+++ test/player_test.rb
@@ -6,3 +6,16 @@ require './lib/ship'
 require './lib/board'
 require './lib/game'
 require './lib/turn'
+
+class PlayerTest < MiniTest::Test
+  def test_it_exists
+    human = Player.new("Teresa")
+
+    assert_instance_of Player, human
+  end
+
+
+
+
+
+end

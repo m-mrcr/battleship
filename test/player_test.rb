22b5b01c5fdcf42b557b5569e9114ce5afdd9064
Teresa Knowles <teresa07moreno@gmail.com>
2018-12-13 01:18:09 -0700

    adds one test for player class

--- test/player_test.rb
+++ test/player_test.rb
@@ -4,8 +4,7 @@ require 'pry'
 require './lib/cell'
 require './lib/ship'
 require './lib/board'
-require './lib/game'
-require './lib/turn'
+require './lib/player'
 
 class PlayerTest < MiniTest::Test
   def test_it_exists
@@ -14,6 +13,16 @@ class PlayerTest < MiniTest::Test
     assert_instance_of Player, human
   end
 
+  def test_computer_places_five_ships
+    computer = Player.new("computer")
+    computer.place_ships_ai
+
+    expected = 5
+    actual = computer.board.cells.values.count {|cell| !cell.empty? }
+
+    assert_equal expected, actual
+
+  end
 
 
 
